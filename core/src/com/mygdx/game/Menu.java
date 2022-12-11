42bec32aa3ba463fc421753cbec4ce85dd4b7043
радон <jf.com@mail.ru>
2022-12-11 20:46:06 +0300

    v0.6 OwlQuest

--- core/src/com/mygdx/game/Menu.java
+++ core/src/com/mygdx/game/Menu.java
@@ -57,7 +57,7 @@ public class Menu implements Screen {
         button.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y){
-                game.setScreen(new GameScreen());
+                game.setScreen(new OwlQuestions(game));
                 dispose();
             }
         });
